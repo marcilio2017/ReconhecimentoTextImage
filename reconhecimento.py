d7497b8a903eab8f44e42172079b377fe1ac6eab
Marcílio Bezerra <marcilio.bc@gmail.com>
2024-05-28 16:27:59 -0300

    :sparkles: feat: API externa
    Utilizando a api: Cloud Vision

--- reconhecimento.py
+++ reconhecimento.py
@@ -1,18 +1,25 @@
 from google.cloud import vision
+from google.oauth2 import service_account
 import io
 
-# Inicializa o cliente da API
-client = vision.ImageAnnotatorClient()
+# Carrega as credenciais do arquivo JSON
+credentials = service_account.Credentials.from_service_account_file("C:\\Users\\marcilio\\Documents\\credenciais\\nome_arquivo.json")
+
+# Inicializa o cliente da API com as credenciais carregadas
+client = vision.ImageAnnotatorClient(credentials=credentials)
 
 # Carrega a imagem
-with io.open('imagens_reais/shel.jpeg', 'rb') as image_file:
+with io.open('imagens_reais/dois_irmaos.jpeg', 'rb') as image_file:
     content = image_file.read()
 image = vision.Image(content=content)
 
-# Chama a API para detecção de rótulos
-response = client.label_detection(image=image)
-labels = response.label_annotations
+# Chama a API para detecção de texto
+response = client.text_detection(image=image)
+texts = response.text_annotations
 
-# Imprime os rótulos detectados
-for label in labels:
-    print(f'Description: {label.description}, Score: {label.score}')
+# Imprime o texto detectado na imagem
+if texts:
+    print("Texto detectado na imagem:")
+    print(texts[0].description)
+else:
+    print("Nenhum texto foi detectado na imagem.")
